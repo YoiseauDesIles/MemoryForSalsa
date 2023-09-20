3e8057f0fe107b1b502048e1b50f1bb1ea91aaaa
YoiseauDesIles <johan.labrado@hotmail.fr>
2023-09-20 11:00:21 +0200

    add board, cards and random images

---
+++ app/src/main/java/android/gameengine/memoryforsalsa/MemoryBoardAdapter.kt
@@ -0,0 +1,64 @@
+package android.gameengine.memoryforsalsa
+
+import android.content.Context
+import android.gameengine.memoryforsalsa.models.BoardSize
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageButton
+import androidx.cardview.widget.CardView
+import androidx.recyclerview.widget.RecyclerView
+import kotlin.math.max
+import kotlin.math.min
+
+
+class MemoryBoardAdapter(
+    private val context: Context,
+    private val boardSize: BoardSize,
+    private val cardImages: List<Int>
+) : RecyclerView.Adapter<MemoryBoardAdapter.ViewHolder>() {
+
+
+    companion object{
+        private const val MARGIN_SIZE = 10
+        private const val TAG = "MemoryBoardAdapter"
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val cardWidth = parent.width / boardSize.getWidth() - ( 2 * MARGIN_SIZE)
+        val cardHeight = parent.height / boardSize.getHeight() - (2 * MARGIN_SIZE)
+        val cardSideLength = min(cardWidth, cardHeight)
+
+        val view : View = LayoutInflater.from(context).inflate(R.layout.memory_card, parent, false)
+        val layoutParams = view.findViewById<CardView>(R.id.cardView).layoutParams as ViewGroup.MarginLayoutParams
+        layoutParams.width = cardSideLength
+        layoutParams.height = cardSideLength
+        layoutParams.setMargins(MARGIN_SIZE, MARGIN_SIZE, MARGIN_SIZE, MARGIN_SIZE)
+
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(position)
+    }
+
+    override fun getItemCount() = boardSize.numCards
+
+    inner  class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        private val imageButton = itemView.findViewById<ImageButton>(R.id.imageButton)
+
+        fun bind(position: Int) {
+
+            imageButton.setImageResource(cardImages[position])
+            imageButton.setOnClickListener{
+                Log.i(TAG, "Clicked on position $position")
+            }
+        }
+
+    }
+
+}
+
+
---
+++ app/src/main/java/android/gameengine/memoryforsalsa/utils/Constants.kt
@@ -0,0 +1,18 @@
+package android.gameengine.memoryforsalsa.utils
+
+import android.gameengine.memoryforsalsa.R
+
+val DEFAULT_ICONS = listOf(
+    R.drawable.ic_case,
+    R.drawable.ic_face,
+    R.drawable.ic_face_2,
+    R.drawable.ic_flower,
+    R.drawable.ic_heart,
+    R.drawable.ic_house,
+    R.drawable.ic_lightning,
+    R.drawable.ic_moon,
+    R.drawable.ic_plane,
+    R.drawable.ic_school,
+    R.drawable.ic_star,
+    R.drawable.ic_wine
+)
